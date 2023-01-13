8e85489916a3a4d6f69cf3c605903d1ff41b8090
wWrongName <name.johnny@mail.ru>
2023-01-13 15:19:05 +0300

    upd: configs

--- setup.bash
+++ setup.bash
@@ -3,13 +3,13 @@
 S_DIR=$( dirname -- "$0"; )
 
 if [ "$1" = "-r" ]; then
-    echo Read configs from $PWD/$S_DIR
+    echo Read from repository
     cp ${S_DIR}/alacritty ~/.config/ -r
     cp ${S_DIR}/nvim ~/.config/ -r
     cp ${S_DIR}/tmux/.tmux.conf ~/
     tmux source-file ~/.tmux.conf
 elif [ "$1" = "-w" ]; then
-    echo Write config into $PWD/$S_DIR
+    echo Write config into repository
     cp ~/.config/alacritty ${S_DIR}/ -r
     cp ~/.config/nvim ${S_DIR}/ -r
     cp ~/.tmux.conf ${S_DIR}/tmux/ 
